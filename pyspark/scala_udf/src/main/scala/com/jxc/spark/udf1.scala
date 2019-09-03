1081ca4d6ea56b3e8babf428964cb6776a07fbe4
jiaxicheng <xichengjia@gmail.com>
2019-09-03 18:44:36 -0400

    added UDF2 with two UDF function arguments

--- pyspark/scala_udf/src/main/scala/com/jxc/spark/udf1.scala
+++ pyspark/scala_udf/src/main/scala/com/jxc/spark/udf1.scala
@@ -1,19 +1,15 @@
 package com.jxc.spark
 
 import org.apache.spark.sql.api.java.UDF1
+import org.apache.spark.sql.api.java.UDF2
 import scala.collection.mutable.ListBuffer
 
-
-/*
-  caseclass: https://stackoverflow.com/questions/45080913/using-spark-udfs-with-struct-sequences
-             https://docs.scala-lang.org/tour/case-classes.html
-  spark-sql-udf: https://stackoverflow.com/questions/38413040/spark-sql-udf-with-complex-input-parameter
-  difference between List and Array: https://stackoverflow.com/questions/2712877/difference-between-array-and-list-in-scala
-
+/* case class to handle pyspark StructType
  */
-
 case class TRow(t: Double, g: Integer)
 
+/* UDF function when there is 1 argument
+ */
 class SetGroupLabel extends UDF1[Seq[Double], Seq[TRow]] {
 
     override def call(arr: Seq[Double]): Seq[TRow] = {
@@ -22,6 +18,26 @@ class SetGroupLabel extends UDF1[Seq[Double], Seq[TRow]] {
         var new_arr = ListBuffer[TRow]() 
         for (x <- arr.sorted) {
             if (x - w0 >= 5.0001) {
+                g = g+1
+                w0 = x
+            }
+            new_arr += TRow(x, g)
+        }
+        return new_arr
+    }
+}
+
+/* UDF function when there are 2 arguments, with thredhold added for flexibility
+ */
+class SetGroupLabel2 extends UDF2[Seq[Double], Integer, Seq[TRow]] {
+
+    override def call(arr: Seq[Double], N: Integer): Seq[TRow] = {
+        var g:Int = 0
+        var w0:Double = 0
+        var new_arr = ListBuffer[TRow]() 
+        val threshold:Double = N.toDouble + 0.001
+        for (x <- arr.sorted) {
+            if (x - w0 >= threshold) {
                 g = g+1
                 w0 = x
             }
